66abd182d83b83af771916a892763fba20bcb853
Caio LacerdaDev <lacerdacaiogabriel@gmail.com>
2024-10-10 09:01:01 -0300

    feat: create new suspect

--- src/routes/suspeitos.routes.js
+++ src/routes/suspeitos.routes.js
@@ -7,7 +7,7 @@ let suspeitos = [
   {
     id: Math.floor(Math.random() * 1000000),
     nome: "P. Diddy",
-    idade: 20,
+    idade: 75,
     envolvido: true,
   },
   {
@@ -25,11 +25,39 @@ let suspeitos = [
   {
     id: Math.floor(Math.random() * 1000000),
     nome: "Slim Shady",
-    idade: 58,
+    idade: 51,
     envolvido: false
   },
 ];
 // Rota para listar todos os suspeitos
 suspeitosRoutes.get("/", (req, res) => {
   return res.status(200).json(suspeitos);
+});
+
+// Rota para cadastrar um novo suspeito
+suspeitosRoutes.post("/", (req, res) => {
+  const { nome, idade, envolvido, descriçãoFisica} = req.body;
+
+  // Validação dos campos obrigatórios
+  if (!nome) {
+    return res.status(400).json({
+      message: "Os campos nome!",
+    });
+  }
+
+  // Criação de um novo suspeito
+  const novoSuspeito = {
+    id: Math.floor(Math.random() * 1000000),
+    nome,
+    idade,
+    envolvido,
+  };
+
+  // Adiciona o novo suspeito ao array de suspeitos
+  suspeitos.push(novoSuspeito);
+
+  return res.status(201).json({
+    message: "suspeito cadastrado com sucesso!",
+    novoSuspeito,
+  });
 });
