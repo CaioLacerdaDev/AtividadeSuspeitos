8b6b38d3696cf79bcbb6f5b423629384582a133e
Caio LacerdaDev <lacerdacaiogabriel@gmail.com>
2024-10-10 08:46:21 -0300

    feat: create routes

---
+++ src/index.routes.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+
+// Lista de importação das rotas do projeto
+import suspeitoRoutes from "./suspeitos.routes.js";
+
+const routes = Router();
+
+// Rota raiz para teste
+routes.get("/", (req, res) => {
+  return res.status(200).json({ message: "Testado fio!" });
+});
+
+// Lista de uso das rotas do projeto
+routes.use("/suspeitos", suspeitoRoutes);
+
+export default routes;
